9c55c3e4cc7278f69b11b99c98ad6e4a256c4801
ParvesPR <parves2585@gmail.com>
2022-03-11 13:33:10 +0600

    qna added

--- js/app.js
+++ js/app.js
@@ -35,16 +35,19 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById('qna-section').style.display="block";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById('qna-section').style.display="none";
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
+        document.getElementById('qna-section').style.display="none";
 
         displayReportedPosts();
     }
@@ -156,6 +159,7 @@ const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
     reportedPosts.forEach((post) => {
         const div = createPost(post);
+        console.log(div)
         document.getElementById( "reported" ).appendChild(div);
     });
 };
